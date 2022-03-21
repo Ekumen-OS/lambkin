277dc6e57eb4723f94bf5b15ef929895413fe30f
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2022-03-21 09:05:52 -0300

    Address code review comments
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

--- src/core/lambkin/src/lambkin/test_case_evaluation.py
+++ src/core/lambkin/src/lambkin/test_case_evaluation.py
@@ -12,16 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
-
 import numpy as np
 
-from dataclasses import dataclass, field
 from itertools import product
 
-from robot.model.testcase import TestCase
-from robot.model.testsuite import TestSuite
-from robot.result.model import TestSuite as TestResult
+from robot.api import TestSuite
+
 from typing import Any
 from typing import Generator
 from typing import Iterable
@@ -29,65 +25,45 @@ from typing import List
 from typing import Tuple
 
 
-@dataclass
-class TestData:
-    """Represents test case data in a way that is easier to handle."""
-
-    name: str
-    keyword: str
-    args: List[str] = field(default_factory=list)
-
-    @staticmethod
-    def from_test_case(test_case: TestCase) -> TestData:
-        """Creates a instance of TestData from a given TestCase."""
-        if len(test_case.body) != 1:
-            raise AssertionError(f'Test case body must have one keyword.')
-        keyword = test_case.body[0]
-        return TestData(
-            name=test_case.name,
-            keyword=keyword.name,
-            args=list(keyword.args))
-
-
 def linspace(start: float, end: float, num: int) -> Generator[float, None, None]:
     """Generates evenly spaced numbers over a specified interval."""
-    for value in np.linspace(start, end, num):
-        yield value
+    yield from np.linspace(start, end, num)
 
 
 def sequence(*args: Any) -> Generator[Any, None, None]:
     """Generates a sequence consisting of the provided values."""
-    for value in args:
-        yield value
+    yield from args
 
 
 class TestCaseEvaluation:
     """A RobotFramework listener that expands
-    generator functions in test case arguments."""
+    generator expressions in test case arguments."""
 
     ROBOT_LISTENER_API_VERSION = 3
 
     def __init__(self):
         self.ROBOT_LIBRARY_LISTENER = self
 
-    def start_suite(self, suite: TestSuite, result: TestResult) -> None:
+    def start_suite(self, suite: TestSuite, _) -> None:
         """Removes all test cases from the suite and creates
         a new list of test cases expanding their arguments."""
-        tests = [TestData.from_test_case(test) for test in suite.tests]
-        suite.tests = None
-
-        for test in tests:
-            for num, args in enumerate(self._generate_args(test), start=1):
+        tests = suite.tests
+        suite.tests = None  # Remove existing test cases
+        for test_case in tests:
+            if len(test_case.body) != 1:
+                raise AssertionError('Test case body must have one keyword.')
+            keyword = test_case.body[0]
+            for num, args in enumerate(self._generate_args(keyword.args), start=1):
                 # TODO(nahuel): find a better naming system
-                test_case = suite.tests.create(name=f'{test.name} {num}')
-                test_case.body.create_keyword(name=test.keyword, args=args)
+                new_test_case = suite.tests.create(name=f'{test_case.name} {num}')
+                new_test_case.body.create_keyword(name=keyword.name, args=args)
 
     @staticmethod
-    def _generate_args(test: TestData) -> Generator[Tuple[Any], None, None]:
+    def _generate_args(args: Iterable[str]) -> Generator[Tuple[Any], None, None]:
         """Generates new argument tuples with the cartesian
         product of evaluated expressions."""
         iterators: List[Iterable[Any]] = []
-        for arg in test.args:
+        for arg in args:
             try:
                 result = eval(arg)
                 if not isinstance(result, Iterable):
@@ -96,6 +72,4 @@ class TestCaseEvaluation:
             except Exception:
                 # If the argument can't be evaluated, keep it as is
                 iterators.append([arg])
-
-        for args in product(*iterators):
-            yield args
+        yield from product(*iterators)
