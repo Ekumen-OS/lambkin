6ce85b331e15c7a3b673e5266f173ecd5a13688e
Michel Hidalgo <michel@ekumenlabs.com>
2026-01-28 20:41:27 +0000

    Bump Earthly to 0.8.17-rc-2 (#128)
    * Bump Earthly to 0.8.17-rc-2
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Update Earthly patch
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Make tools/setup.sh idempotent
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    ---------
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,7 +4,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-EARTHLY_VERSION=v0.8.14
+EARTHLY_VERSION=v0.8.17-rc-2
 
 case $(uname -s) in
     Linux*)     KERNEL=linux;;
@@ -23,15 +23,13 @@ esac
 
 pushd ${SCRIPT_DIR}
 
-if [ ! -d ./earthly-src ]; then
-    git clone -b ${EARTHLY_VERSION} https://github.com/earthly/earthly.git earthly-src
-    git -C ./earthly-src apply ${SCRIPT_DIR}/earthly.patch
-fi
+[ -d ./earthly-src ] && rm -rf ./earthly-src
+git clone -b ${EARTHLY_VERSION} https://github.com/EarthBuild/earthbuild.git earthly-src
+git -C ./earthly-src apply --3way ${SCRIPT_DIR}/earthly.patch
 
-if [ ! -x ./earthly-bootstrap ]; then 
-    wget -O ./earthly-bootstrap https://github.com/earthly/earthly/releases/download/${EARTHLY_VERSION}/earthly-${KERNEL}-${ARCH} 
-    chmod +x ./earthly-bootstrap
-fi
+[ -f ./earthly-bootstrap ] && rm ./earthly-bootstrap
+wget -O ./earthly-bootstrap https://github.com/EarthBuild/earthbuild/releases/download/${EARTHLY_VERSION}/earthly-${KERNEL}-${ARCH}
+chmod +x ./earthly-bootstrap
 
 ./earthly-bootstrap ./earthly-src+for-own
 ln -sTf ./earthly-src/build/own/earthly ./earthly
