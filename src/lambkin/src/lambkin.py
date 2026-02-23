3a0ee145bf5931cb91ace59b1ef37270bea5531d
Maria Teresa Ortega <teresa.ortega0903@gmail.com>
2026-02-23 16:20:15 +0100

    Complete script structure and start working on the launcher
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>

--- src/lambkin/src/lambkin.py
+++ src/lambkin/src/lambkin.py
@@ -8,56 +8,72 @@ import time
 from pathlib import Path
 # User defined variables
 input_file = "/ws/lambkin/input/hallway_localization"
-output_path = "/ws/lamkin/output/"
+output_path = "/ws/lambkin/output/"
 num_iterations = 0
 clock_rate = 10
 qos_file_path = "/ws/beluga/beluga_example/bags/qos_override.yaml"
 # Options for the benchmark
 laser_models = ["likelihood", "beam"]
 num_particles = [1, 10, 100, 1000, 10000]
-clock_rate_options = ["--clock-rate", clock_rate]
+clock_rate_options = ["--clock-rate", str(clock_rate)]
 qos_options= ["--qos-profile-overrides-path", qos_file_path]
-topics_interested = ["/pose", "/tf", "/tf_tatic"]
+clock_option = "--clock"
+topics_interested = ["/pose", "/tf", "/tf_static"]
 
-def execute_background_process(full_cmd_list: list[str]) :
+def execute_background_process(full_cmd_list: list[str], log_file: str = None) -> subprocess.Popen :
     print(f"{full_cmd_list}\n")
-    archivo_log = open("beluga.log", "w")
+    if log_file:
+        file = open(log_file, "w")
+        return subprocess.Popen(full_cmd_list,stdout=file,stderr=file) 
     return subprocess.Popen(full_cmd_list)
-    #    stdout=archivo_log,  # Guarda los textos normales
-    #     stderr=archivo_log)
     
 
 def ros_bag_record(output_path:str, options: list[str]) -> subprocess.Popen:
+    bag_dir = output_path / "bag"
+    bag_dir.mkdir(parents=True, exist_ok=True)
     return execute_background_process(["ros2", "bag", "record", "-o", output_path ]+ options)
     
 def ros_bag_play(input_path: str, options: list[str]) -> subprocess.Popen:
     return execute_background_process(["ros2", "bag", "play",input_path ]+ options)
     
 def beluga(sensor_model, num_particles,map_path: str ) -> subprocess.Popen:
-    cmd_list = [ "ros2", "launch", "beluga_example","localization_launch.py" , f"localizacion_map:={map_path}" , f"laser_model_type:={sensor_model}", f"max_particles:={num_particles}" ]
-    return execute_background_process(cmd_list)
+    cmd_list = [ "ros2", "launch", "beluga_example","localization_launch.py" , 
+                f"localizacion_map:={map_path}" , 
+                f"laser_model_type:={sensor_model}", 
+                f"max_particles:={num_particles}" ]
+    return execute_background_process(cmd_list, log_file="beluga.log")
 
-def bag2tum(record_path: str, output_tum_path: str, topics) -> str:
-    Path(output_tum_path).parent.mkdir(parents=True, exist_ok=True)
+def bag2tum(bag_path: str, tum_path: str, topic: str) -> str:
+    tum_dir = Path(tum_path) / "tum"
+    tum_dir.mkdir(parents=True, exist_ok=True)
+
     cmd_list = [
-        "evo_traj", "bag2", record_path, topics, 
+        "evo_traj", "bag2", str(bag_path), topic, 
         "--save_as_tum"
     ]
-    # subprocess.run(cmd_list, check=True)   
-    return output_tum_path
+    subprocess.run(cmd_list, cwd=tum_dir, check=True)   
+    
+    tum_name = f"{topic.strip('/')}.tum"
+    return str(tum_dir / tum_name)
 
 
-def evo_ape(reference_path: str, compare_bag_path: str, output_zip_path: str):
-    tum_path = compare_bag_path.replace(".bag", ".tum")  
-    compare_path = bag2tum(compare_bag_path, tum_path)
-    Path(output_zip_path).parent.mkdir(parents=True, exist_ok=True)
+def evo_ape(reference_path: str, record_path: str, topic: str = "/pose"):   
+    
+    iter_dir = record_path.parent.parent
+    
+    record_tum = bag2tum(str(record_path), str(iter_dir), topic)
+    
+    ape_dir = iter_dir / "ape"
+    ape_dir.mkdir(parents=True, exist_ok=True)
+
     cmd_list = [
         "evo_ape", "tum", 
-        reference_path, compare_path, 
+        reference_path, record_tum, 
         "-va", "--align", 
-        "--save_results", output_zip_path
+        "--save_results", str(ape_dir / "ape.zip") 
     ]
-    # subprocess.run(cmd_list, check=True)
+    
+    subprocess.run(cmd_list, check=True)
 
 
 def wait_for_processes(waitlist: list[subprocess.Popen], termination_list: list[subprocess.Popen]):
@@ -77,8 +93,9 @@ def wait_for_processes(waitlist: list[subprocess.Popen], termination_list: list[
     
 def plot_ape_metrics(path_ape):
     cmd_list = ["evo_res", path_ape]
-    #subprocess.run(cmd_list)
     print(f"{cmd_list}\n")
+    subprocess.run(cmd_list)
+   
 
 
 def make_variations() -> list:
@@ -93,15 +110,17 @@ def make_variations() -> list:
                 
 
 def run_iteration(variation: dict, iteration):
-    nombre_variacion = f"{variation["sensor_model"]}_p{variation["num_particles"]}"
-    base_dir = Path("benchmarking") / nombre_variacion / f"iter_{iteration}"
-    bag_dir = base_dir / "bag"
-    bag_dir.mkdir(parents=True, exist_ok=True)
+    nombre_variacion = f"{variation['sensor_model']}_p{variation['num_particles']}"
+    base_dir = Path(output_path) / "benchmarking" / nombre_variacion / f"iter_{iteration}"
+
+    
     p_beluga = beluga(variation["sensor_model"], variation["num_particles"], "/ws/lambkin/input/map.yaml")
     time.sleep(3)
 
-    p_play = ros_bag_play(input_file , [clock_rate_options, qos_options])
-    p_record = ros_bag_record("/ws/lamkin/results/output", topics_interested)
+    p_play = ros_bag_play(input_file, clock_rate_options + qos_options)
+    
+    p_record = ros_bag_record(str(base_dir), topics_interested)
+    
     wait_for_processes([p_play], [p_beluga, p_record])
 
 
@@ -133,23 +152,15 @@ class TestProcessManagement(unittest.TestCase):
         pass
 
 def main():
-    print("ejecucion")
-    for variation in make_variations():
-        # for it in range(num_iterations):
-        print(f"{variation}\n")
-        run_iteration(variation, 0)
-        time.sleep(10000)
-    #evo_ape(3)
-    #plot_ape_metrics(3)
-    #plot_ape_metrics(variation)
 
+    for variation in make_variations():
+        for it in range(num_iterations):
+            run_iteration(variation, it)
+            time.sleep(2) 
 
 if __name__ == "__main__":
-
     if len(sys.argv) > 1 and sys.argv[1] == "test":
-       
         sys.argv.pop() 
         unittest.main()
-        
     else:
         main()
