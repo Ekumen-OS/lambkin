7ed50e48c2bd435577fe82c7194af9f2347c2496
Maria Teresa Ortega <teresa.ortega0903@gmail.com>
2026-03-10 15:16:39 +0100

    Delete folder src/lambkin/core/ctx/shell
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>

--- src/lambkin/core/ctx/shell/proxy.py
+++
@@ -1,6 +0,0 @@
-"""Shell proxy for lambkin benchmark execution.
-
-Provides an abstraction over shell command dispatch,
-allowing benchmark processes to be launched and managed
-through a consistent interface.
-"""
