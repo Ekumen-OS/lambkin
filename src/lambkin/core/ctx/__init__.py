0b100354384375e11ba0ba7823a0f0d7acf06843
TeresaOrtega <94405914+teresa-ortega@users.noreply.github.com>
2026-03-18 15:24:47 +0100

    Phase 3: core/ctx/context and core/ctx/source (#134)
    * Add inicial context structure
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Context create folders
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Shell object
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Change /ape for metrics
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Add test file
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Test file
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Add new option implementation
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Remove OptionsInfo class
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * New implementation Variations and folders
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * ROS comments
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Inputs and source info
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Variations and options methods deleted
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix pytest
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * New folders structure
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * New source and input implementation
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Address some reviews
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Address some source reviews
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix docstrings
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix source test
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix pytest problem
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix iteration problem
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Iteration fix
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix test and folders
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix some typo
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Fix typo
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * Address some reviews
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    * source test
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    
    ---------
    
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>
    Signed-off-by: Gerardo Puga <glpuga@gmail.com>

--- src/lambkin/core/ctx/__init__.py
+++ src/lambkin/core/ctx/__init__.py
@@ -14,6 +14,14 @@
 
 """Context management for lambkin benchmark runs.
 
-Provides the benchmark execution context and shell utilities for process
-interaction and proxy handling.
+Provides the benchmark execution context and source information
+for a single (variation, iteration) run.
 """
+
+from .context import Context
+from .source import Source
+
+__all__ = [
+    "Context",
+    "Source",
+]
---
+++ test/core/test_source.py
@@ -0,0 +1,50 @@
+# Copyright 2026 Ekumen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for the source function in lambkin.core.ctx."""
+
+from pathlib import Path
+
+from lambkin.core.ctx.source import Source
+
+
+def test_source_path_is_set_correctly():
+    """ctx.source.path reflects the path passed at construction."""
+    test_file = Path(__file__)
+    source = Source(path=test_file)
+    assert source.path == test_file
+
+
+def test_source_path_is_converted_to_path():
+    """Source converts a string path to a Path object."""
+    test_path_str = "/home/user/benchmarks/test.py"
+    source = Source(path=test_path_str)
+    assert isinstance(source.path, Path)
+    assert source.path == Path(test_path_str)
+
+
+def test_source_parent_anchor():
+    """Verify that source.path.parent can be used to anchor relative paths."""
+    test_path = Path("/home/user/benchmarks/my_benchmark.py")
+    source = Source(path=test_path)
+    dataset_path = source.path.parent / "datasets" / "output.mcap"
+
+    assert dataset_path == test_path.parent / "datasets" / "output.mcap"
+
+
+def test_source_repr():
+    """Source.__repr__ returns a human-readable string."""
+    test_path = Path("/home/user/benchmarks/test.py")
+    source = Source(path=test_path)
+    assert repr(source) == f"Source(path={test_path})"
