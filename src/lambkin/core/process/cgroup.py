0a00389d05399b574e9290b7ed6681c7fa931c29
Maria Teresa Ortega <teresa.ortega0903@gmail.com>
2026-05-28 16:15:51 +0200

    Fix EBUSY
    Signed-off-by: Maria Teresa Ortega <teresa.ortega0903@gmail.com>

--- src/lambkin/core/process/cgroup.py
+++ src/lambkin/core/process/cgroup.py
@@ -145,7 +145,7 @@ def remove_cgroup(cgroup: Path) -> None:
             cgroup.rmdir()
             return
         except OSError as e:
-            if e.errno == errno.ENOTEMPTY:
+            if e.errno == (errno.ENOTEMPTY, errno.EBUSY):
                 time.sleep(defaults.CGROUP_POLL_INTERVAL)
             elif e.errno == errno.ENOENT:
                 return
